1d6d7fb273c0d7f5292a0d7b4a1f760140b80d2b
Kovacs Tamas <kovacstamas990@gmail.com>
2024-09-07 09:42:32 +0200

    Userinfo is commented out

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,28 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { TailSpin } from "react-loader-spinner";
+import EventMobile from "./EventMobile";
 
-function Home() {
+const Home = (props)=> {
+
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // const url = "https://budapestgayguide-backend.onrender.com";
+  const url ="http://localhost:8080"
+
+
+  const getEventData = () => {
+    axios.get(url + "/getevents").then((response) => {
+      console.log(response)
+      setEventData(response.data)
+      setLoading(false);
+    });
+  };
 
   return (
-    <div className="Home">
-      HOME
-    </div>
+      <div className="home">
+        <EventMobile eventData={eventData}></EventMobile>
+      </div>
   );
 }
 
